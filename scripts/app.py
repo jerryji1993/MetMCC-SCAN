68aaceb42e24c522a1ecd2afc4149ea8890662f4
jerryji1993 <yanrongji2021@u.northwestern.edu>
2020-09-23 15:46:42 -0500

    update scripts

---
+++ scripts/app.py
@@ -0,0 +1,13 @@
+# general modules
+import os
+from copy import deepcopy
+import pickle as pkl
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+# load model
+from imblearn.ensemble import BalancedRandomForestClassifier
+
