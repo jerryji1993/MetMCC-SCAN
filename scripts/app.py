77ee8b5fc443b6e69ff7cbd5fcb8762d2a9ce5e4
jerryji1993 <yanrongji2021@u.northwestern.edu>
2020-09-24 14:04:58 -0500

    update streamlit app

--- scripts/app.py
+++ scripts/app.py
@@ -8,11 +8,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 from PIL import Image
+from load_css import local_css
 
 # imblearn model
 from imblearn.ensemble import BalancedRandomForestClassifier
 
-MODEL_DIR = '../model/balanced_rf_imputed_binned.pkl'
+
+
+MODEL_DIR = '../model/balanced_rf_knnimputed_binned.pkl'
+DATA_DIR = '../data/Merkel_NCDB_clean_selectedVars_imputed.pkl'
+IMPUTER_DIR = '../model/knn_imputer.pkl'
 
 @st.cache(allow_output_mutation=True)
 def load_model():
@@ -20,15 +25,21 @@ def load_model():
         model = pkl.load(f)
     return model
 
+@st.cache(allow_output_mutation=True)
+def load_imputer():
+    with open(IMPUTER_DIR, 'rb') as f:
+        imputer = pkl.load(f)
+    return imputer
+
 @st.cache
 def load_data():
-    with open('../data/Merkel_NCDB_clean_selectedVars.pkl', 'rb') as f:
+    with open(DATA_DIR, 'rb') as f:
         full_data = pkl.load(f)
     return full_data
 
 def get_sidebar_input():
     # demographic
-    age = st.sidebar.slider('Age of patient: ', min_value= 20, max_value=100, value=70, step=1)
+    age = st.sidebar.slider('Age of patient: ', min_value= 20, max_value=100, value=60, step=1)
     gender = st.sidebar.radio(label="Gender of patient: ", options = ('Male', 'Female'))
 
     # primary site
@@ -42,7 +53,7 @@ def get_sidebar_input():
         tumor_size = st.sidebar.number_input('''Largest dimension of diameter of the primary tumor in 
                         millimeters (mm):''', min_value = 0, max_value=995, value=1,step = 1)
     else:
-        tumor_size = 0
+        tumor_size = 999
 
     # depth
     depth_known = st.sidebar.checkbox('Have information about thickness/depth of primary tumor?')
@@ -50,7 +61,7 @@ def get_sidebar_input():
         depth = st.sidebar.number_input('''Measured thickness (depth) of primary tumor in 
                         millimeters (mm):''', min_value = 0, max_value=980, value=1,step = 1)
     else:
-        depth = 0
+        depth = 999
 
     # lymph vascular invasion
     LVI = st.sidebar.radio(label="Tumor cells present in lymphatic channels/blood vessels within the primary tumor: ", 
@@ -111,7 +122,12 @@ def align_data(df, new_df):
     df, new_df = df.align(new_df, join='left', axis=1)
     new_df.drop('SLNB', axis=1, inplace=True)
     new_df = new_df.fillna(0)
+    new_df = new_df.replace([999],[np.NaN])
     return new_df
+
+def impute(imputer, df):
+    return imputer.transform(df)
+    
 
 def main():
     st.title('MetMCC-SCAN')
@@ -126,6 +142,9 @@ def main():
     
 #     st.dataframe(inputs)
     if st.button("Predict"):
+        # load imputer
+        imputer = load_imputer()
+        inputs = impute(imputer, inputs)
         # load model
         model = load_model()
         y_prob = np.asarray(model.predict_proba(inputs))
@@ -133,13 +152,34 @@ def main():
 
         # output prediction
         st.write("Probability of having a positive biopsy:", y_prob[:,1][0])
+        local_css("style.css")
         if y_pred[0] == 0:
-            st.write("The patient is likely to have a negative Sentinel Lymph Node Biopsy result.")
+
+            t = '''<div>The patient is likely to have a 
+                    <span class='highlight blue'>negative </span> 
+                    Sentinel Lymph Node Biopsy result.
+                   </div>
+                '''
+
         else:
-            st.write("The patient is likely to have a positive Sentinel Lymph Node Biopsy result.")
+            t = '''<div>The patient is likely to have a 
+                    <span class='highlight red'>positive </span>  
+                    Sentinel Lymph Node Biopsy result.
+                   </div>
+                '''
+
+            
+        st.markdown(t, unsafe_allow_html=True)
+        st.markdown("""<br>""", unsafe_allow_html=True)
+        
     image = Image.open('../image/lymph_nodes.jpg')
     st.image(image, use_column_width=True)
     
+#     st.markdown("""<br>""", unsafe_allow_html=True)
+#     st.markdown("""<br>""", unsafe_allow_html=True)
+#     st.markdown("""<br>""", unsafe_allow_html=True)
+#     st.markdown("""<iframe src="https://docs.google.com/presentation/d/1--eW4tCH3lwxLpfyjghiqK3en7VOY016BZvjH87k4mw/embed?start=false&loop=false&delayms=10000" frameborder="0" width="480" height="299" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>""", unsafe_allow_html=True
+#     )
 
 if __name__ == "__main__":
     main()
