1a464b5dbad613835b25a153a0bb5992bb417c1c
jerryji1993 <yanrongji2021@u.northwestern.edu>
2020-10-19 23:07:14 -0500

    add src/config.py

---
+++ src/config.py
@@ -0,0 +1,9 @@
+FEATURES = ['AGE','SEX','PRIMARY_SITE',
+                        'TUMOR_SIZE', 'DEPTH', 
+                        'LYMPH_VASCULAR_INVASION', 
+                        'TUMOR_INFILTRATING_LYMPHOCYTES',
+                        'IMMUNE_SUPPRESSION', 
+                        'GROWTH_PATTERN',
+                        'TUMOR_BASE_TRANSECTION',
+                        'SLNB'
+                       ]
